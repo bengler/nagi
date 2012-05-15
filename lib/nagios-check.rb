a7d6c8e1e212b2f71067b356d7ec7a50da843a7b
Erik Grinaker <erik@bengler.no>
2012-05-15 14:17:06 +0200

    Added command-line option processing.

--- lib/nagios-check.rb
+++ lib/nagios-check.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'optparse'
 
 require 'nagios-check/check'
 require 'nagios-check/status'
--- lib/nagios-check/check.rb
+++ lib/nagios-check/check.rb
@@ -5,6 +5,19 @@ module NagiosCheck
     def initialize(name, version)
       @name = name
       @version = version
+      @optionparser = OptionParser.new do |o|
+        o.banner = "Usage: #{$0} [options]"
+
+        o.on_tail("-V", "--version", "Display version") do
+          puts "#{@name} #{@version}"
+          exit
+        end
+
+        o.on_tail("-h", "--help", "Display this help message") do
+          puts o
+          exit
+        end
+      end
     end
 
     def check
@@ -17,19 +30,25 @@ module NagiosCheck
       return output.strip
     end
 
-    def run
+    def run(args=[])
+      @optionparser.parse!(args)
       begin
-        status = self.check
+        status = self.check(*args)
         raise 'Check did not return a status' unless status.is_a? NagiosCheck::Status
-      rescue Exception => e
+      rescue ArgumentError
+        puts "Error: required arguments not given"
+        puts
+        puts @optionparser
+        exit 3
+      rescue StandardError => e
         status = Unknown.new(e.message)
       end
       return status
     end
 
     def run!
-      status = self.run
-      puts status
+      status = self.run(ARGV)
+      puts "#{@name.upcase} #{status}"
       exit status.code
     end
   end
