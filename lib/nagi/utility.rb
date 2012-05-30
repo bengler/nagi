36a269ca7363079d53b49f8e9cdff3ddd04decce
Erik Grinaker <erik@bengler.no>
2012-05-30 20:17:37 +0200

    Made Utility::execute() work with Ruby 1.8.

--- lib/nagi/utility.rb
+++ lib/nagi/utility.rb
@@ -3,7 +3,14 @@ module Nagi
     module_function
 
     def execute(command)
-      output, status = Open3.capture2e("/bin/bash -o pipefail -c '#{command}'")
+      if defined?(Open3.capture2)
+        output, status = Open3.capture2e("/bin/bash -o pipefail -c '#{command}'")
+      else
+        IO.popen("/bin/bash -o pipefail -c '#{command}' 2>&1") do |io|
+          output = io.read
+        end
+        status = $?
+      end
       raise "Shell failure, '#{output.gsub(/^\/bin\/bash: /, '').strip}'" if status.exitstatus != 0
       return output.strip
     end
